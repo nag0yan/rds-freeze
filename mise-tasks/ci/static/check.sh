7cb94eb899c2a3e7b9aca1f400769bde2f6168e3
nag0yan <matsui-ka@kadokawa.jp>
2025-10-30 03:04:10 +0900

    shellcheck

--- mise-tasks/ci/static/check.sh
+++ mise-tasks/ci/static/check.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 actionlint
 pinact run -check
+find . -name '*.sh' -exec shellcheck {} +
+find . -name '*.sh' -exec shfmt -d {} +
 go vet ./...
 go tool staticcheck ./...
 gofmt -d .
--- mise-tasks/ci/static/fix.sh
+++ mise-tasks/ci/static/fix.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 actionlint
 pinact run
+find . -name '*.sh' -exec shellcheck {} +
+find . -name '*.sh' -exec shfmt -w {} +
 go vet ./...
 go tool staticcheck ./...
 gofmt -w .
